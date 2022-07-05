208f0b32195c411c17b2d45f71cd72be4b16d9c4
SergeyMetelskiy <108751783+SergeyMetelskiy@users.noreply.github.com>
2022-07-05 21:43:09 +0300

    Lesson 3

---
+++ les3.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class les3 {
+
+    public static void main(String[] args){
+      /* //Boolean1◦
+       // Дано целое число A. Проверить истинность высказывания: Число A является положительным
+        Scanner in =new Scanner(System.in);
+        int a=in.nextInt();
+        boolean res=a>0;
+        System.out.println(res);*/
+       /* //Boolean2. Дано целое число A. Проверить истинность высказывания: «Число A является нечетным».
+        /System.out.println("Введите целое число:");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+    boolean res = a % 2 == 0;
+    System.out.print(res);*/
+       /* //Boolean4. Даны два целых числа: A, B. Проверить истинность высказывания:
+         //   «Справедливы неравенства A > 2 и B ≤ 3»
+        // Логическое И && - "Логическое умножение"
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        boolean res = a>2 && b<=3;
+        System.out.print(res);*/
+       /*// Boolean.Даны два целых числа: A, B. Проверить истинность высказывания:
+            //«Справедливы неравенства A ≥ 0 или B < −2».
+        //System.out.print(res);
+        Scanner on = new Scanner(System.in);
+        int a = on.nextInt();
+        int b = on.nextInt();
+        int c = on.nextInt();*/
+        //Boolean11. Даны два целых числа: A, B. Проверить истинность высказывания: «Числа A и B имеют одинаковую четность»
+        // Scanner on = new Scanner(System.in);
+        /* int a = on.nextInt();
+        int b = on.nextInt();
+        boolean res = (a%2)==(b%2);
+        System.out.println(res);
+        Boolean.res = (a % 2 != 0 && b % 2 != 0);
+        System.out.println(res);*/
+
+    }
+}
